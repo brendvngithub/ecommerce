c4992a3107e047d6d138570d5bfecea2cf302f98
Brend <brend@swordfish.co.za>
2019-03-17 11:23:54 +0200

    added webpack

---
+++ resources/assets/js/app.js
@@ -0,0 +1,21 @@
+window .$ = window.jQuery = require('jquery');
+window.axios = require('axios');
+window.Vue = require('vue');
+
+require('foundation-sites/dist/js/foundation.min');
+
+
+require('slick-carousel/slick/slick.min');
+require('chart.js/dist/Chart.bundle');
+
+require('../../assets/js/acme');
+require('../../assets/js/admin/create');
+require('../../assets/js/admin/dashboard');
+require('../../assets/js/admin/delete');
+require('../../assets/js/admin/event');
+require('../../assets/js/pages/chart');
+require('../../assets/js/pages/home_products');
+require('../../assets/js/pages/lib');
+require('../../assets/js/pages/product_details');
+require('../../assets/js/pages/slider');
+require('../../assets/js/init');
